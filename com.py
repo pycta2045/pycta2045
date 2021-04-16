7e95c2a57b93dccc83cffc26b8fa5ae1e58fa8c9
mohamm-alsaid <mohamm.alsaid@gmail.com>
2021-04-16 15:20:01 -0700

    add csv files

--- com.py
+++ com.py
@@ -1,6 +1,8 @@
 import serial
 import serial.tools.list_ports
-port = "/dev/pts/4"
+port = "/dev/ttyS6"
+ports = serial.tools.list_ports.comports()
 with serial.Serial(port) as ser:#,num,timeout=0) as ser:
-    x = ser.readline()
+    x = ser.read(10)
     print(f'{x}')
+#print(ports)
