6b2cd30a54eb9dc455277e7f16f4aab236161f2d
mohamm-alsaid <mohamm.alsaid@gmail.com>
2021-04-23 11:52:04 -0700

    add simple listener

--- DER_agent.py
+++ DER_agent.py
@@ -1,4 +1,6 @@
 from agents.scpi.handler import SCPI
+import sys
+import argparse
 from agents.gen_agent import generic_agent
 from agents.cta2045.handler import command_handler
 from agents.com.handler import COM
@@ -14,12 +16,34 @@ from agents.com.handler import COM
 # r = scpi.send('lock screen')
 # DER_agent = generic_agent('file.json')
 # print(DER_agent.get_state())
-# cta = command_handler(fname="agents/cta2045/CTA2045_commands.json")
-# cmds = cta.dump_commands()
-# shed = cta.get("shed")
-# print(cmds)
+
 # print(f'shed: {shed}')
 # # DER_agent.charge()
 
-print('--')
-com = COM()
+parser = argparse.ArgumentParser(description='Process program args.')
+parser.add_argument('-p', type=str, help='port to use',default='/dev/ttyS5')
+parser.add_argument('-m', type=bool, help='mode {True: listen, False: send}',default=False)
+# parser.add_argument('-c', type=str, help='command to send',default='shed')
+
+
+
+
+args = parser.parse_args()
+port = args.p
+# cmd = args.c
+listen = args.m
+
+print(listen)
+com = COM(port)
+cta = command_handler(fname="agents/cta2045/CTA2045_commands.json")
+cmds = cta.dump_commands()
+    
+if listen:
+    # while input() != 'q':
+    r = com.recv()
+    print(f'recv: {r}')
+else:
+    shed = cta.get("shed")
+    com.send(shed)
+    print(f'sent: {shed}')
+    # r = com.recv()
--- agents/com/handler.py
+++ agents/com/handler.py
@@ -1,16 +1,24 @@
 import serial
 from serial.tools.list_ports import comports
+import io
 
 class COM:
     def __init__(self, port="/dev/ttyS6"):
         self.port = port
-        self.verify_port()
+        if not self.verify_port():
+            raise Exception(f"port {self.port} not found")
+        # TODO: handle errors
+        self.ser = serial.Serial(self.port)
+        self.sio = io.TextIOWrapper(io.BufferedRWPair(self.ser,self.ser))
         return
     def verify_port(self):
-        ports = list(comports())
-        print(ports)
+        ports = list(map(lambda x: x[0],comports()))
         return self.port in ports
-    def get_next(self):
-        
+    def send(self,data):
+        print('wrote= ',self.sio.write(f'{data}\n'))
+        self.sio.flush()  
         return
+    def recv(self):
+        data = self.ser.readline()
+        return data.decode()
         
