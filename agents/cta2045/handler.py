cecbdaa5134549977ed537ed35d9d12749cc6354
moh <mohamm.alsaid@gmail.com>
2021-07-19 02:33:01 -0700

    add more commands to CTA2045 commands (json)

--- agents/cta2045/handler.py
+++ agents/cta2045/handler.py
@@ -1,5 +1,12 @@
 import json
 import os
+
+
+# @TEST:
+# 1. create basic message & test length against combined length given from dict
+# 2. create data-link message & test length against combined length given from dict
+# 3. acks/naks should be 2 bytes only
+
 class command_handler:
     def __init__(self,fname="CTA2045_commands.json"):
         self.cmds = {}
@@ -12,14 +19,19 @@ class command_handler:
         return
     def dump_commands(self):
         return self.cmds
-    def get_hex(self,cmd):
+    def to_cta(self,cmd):
+        # TODO: 
+        # 1. query MsgType from dict
+        # 2. query msg from dict
+        # 3. call checksum on msg
+
         v = hex(0)
         try:
             v = f"{self.cmds['op code']} {self.cmds[cmd]}"
         except Exception as e:
             print(e)
         return v
-    def get_key(self,val):
+    def from_cta(self,hex):
         key = None
         for k,v in self.cmds.items():
             if v == val:
