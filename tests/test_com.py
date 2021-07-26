cf32e67cc50c3dfa5417378aacf2cbe29600c49b
moh <mohamm.alsaid@gmail.com>
2021-07-26 15:42:12 -0700

    update com mod unit test

--- tests/test_com.py
+++ tests/test_com.py
@@ -1,15 +1,16 @@
 import unittest
 import agents.com.handler as COM
 import serial.tools.list_ports as lp
+from agents.cta2045.handler import CTA2045
 
-
+cta = CTA2045()
 class TestCOM(unittest.TestCase):
     def testPortExistFail(self):
         port = 'fake'
         msg = ''
         c = None
         try:
-            c = COM.COM(port=port)
+            c = COM.COM(port=port,checksum=cta.checksum,transform=cta.hexify)
         except Exception as e:
             msg = e.args[0]
         self.assertTrue('not found' in msg)
