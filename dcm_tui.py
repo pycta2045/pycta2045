93b7edae87e9a7f15397d251ede2d3b3b8957587
mohamm-alsaid <mohamm.alsaid@gmail.com>
2021-08-14 13:08:01 -0700

    make Tui send CTA2045 commands

--- dcm_tui.py
+++ dcm_tui.py
@@ -5,7 +5,6 @@ from pycta2045 import cta2045
 from pycta2045 import com
 import sys, os, pandas as pd, time,  select, traceback as tb, multiprocessing#, import threading
 from datetime import datetime
-
 from time import sleep
 
 from rich.align import Align
@@ -14,20 +13,21 @@ from rich.layout import Layout
 from rich.live import Live
 from rich.text import Text
 
-
-
-class DCM:
+num_color = 'red'
+text_color = 'cyan'
+input_color = 'magenta'
+warning_color = 'bright_red'
+class DCM: 
     prompt = {
-        0:"quit",
-        1:"shed",
-        2:"endshed",
-        3:"loadup",
-        4: "critical peak event",
-        5:"grid emergency",
-        6:"operating status request",
+        0: 'quit',
+        1: 'shed',
+        2: 'endshed',
+        3: 'loadup',
+        4: 'critical peak event',
+        5: 'grid emergency',
+        6: 'operating status request',
     }
-    """Renders the time in the center of the screen."""
-    def __init__(self):
+    def __init__(self,port='/dev/ttyS6'):
         self.log = multiprocessing.Queue()
         self.counter = 0
         self.console = Console()
@@ -37,28 +37,61 @@ class DCM:
             Layout(size=10, name="Input"),
         )
         self.layout["main"].split_row(Layout(name="Log"), Layout(name="body", ratio=2))
-        self.counter = 0
+        self.cta_mod = cta2045.CTA2045()
+        self.com = com.COM(checksum=self.cta_mod.checksum,transform=self.cta_mod.hexify,is_valid=self.cta_mod.is_valid,port=port)
+        self.plain_prompt  = list(map(lambda x: f"[{num_color}]{x[0]}[/{num_color}]: [{text_color}]{x[1]}[/{text_color}]",self.prompt.items()))
+        self.plain_prompt = "\n".join(self.plain_prompt)
+        self.pretty_prompt = Text("").from_markup(self.plain_prompt)
         pass
     def write(self,msg,log=False,end='\n'):
         if log:
             self.log.put(msg)
             print(",",msg,log,"\nss\n")
         print(msg)
+        return
     def render(self) -> Layout:
         self.counter+= 1
-        self.layout['body'].update(Text(f"this is a text{self.counter}",style="green"))
-        self.layout['Input'].update(Text(self.console.input("henre"),style="red"))
+        self.layout['body'].update(Text(f"\n\nthis is a text{self.counter}",style="green"))
+        self.layout['Input'].update(self.pretty_prompt)
         return self.layout
+    def update_input_text(self,pretty_text):
+        prompt = f"{self.plain_prompt}\n{pretty_text}"
+        self.pretty_prompt = self.pretty_prompt.from_markup(prompt)
+        self.render()
+        return
+    def send_command(self,cmd):
+        # convert to cta
+        cmd_bytes = self.cta_mod.to_cta(cmd)
+        self.com.send(cmd_bytes)
+        return
 
-
+def validate_input(text,dcm) -> str:
+    if text.isdigit():
+        num = int(text)
+        if num in dcm.prompt:
+            return num 
+    return -1 
 
 def main():
     dcm = DCM()
-
-    with Live(dcm.render(), screen=True, redirect_stderr=False) as live:
+    con = Console()
+    with Live(dcm.render(), screen=True, redirect_stderr=False,refresh_per_second=20) as live:
         try:
             while True:
-                live.update(dcm.render())
+                usr_input = select.select([sys.stdin],[],[],0)
+                if usr_input[0]:
+                    usr_input = sys.stdin.read(1)
+                    sys.stdin.flush()
+                    valid_input = validate_input(usr_input,dcm)
+                    if valid_input > 0:
+                        cmd = dcm.prompt[valid_input]
+                        dcm.send_command(cmd)
+                        text  = f"[b]sending [{input_color}]{cmd}[/{input_color}]..."
+                    elif valid_input == 0:
+                        break
+                    else:
+                        text = f"[b][{warning_color}]INVALID INPUT[/{warning_color}]"
+                    dcm.update_input_text(text)
         except KeyboardInterrupt:
             pass
     # output log
--- pycta2045/cta2045device.py
+++ pycta2045/cta2045device.py
@@ -239,3 +239,222 @@ class CTA2045Device:
         else:
             raise UnknownModeException(f'Unknown Mode: {self.mode}')
         return self.get_log()
+
+# @TODO: give the class below a simpler interface that makes it easier to use
+# ============================= combine packages ==================================
+class Device:
+    def __init__(self,mode='DCM',timeout=1,model=None,comport='/dev/ttyS6'):
+        self.mode = mode.upper()
+        self.model = model
+        self.log = multiprocessing.Queue()
+        self.cta_mod = CTA2045()
+        self.com = COM(checksum=self.cta_mod.checksum,transform=self.cta_mod.hexify,is_valid=self.cta_mod.is_valid,port=comport)
+        self.last_command = '0x00'
+        # flags for minimum cta2045 support
+        self.support_flags = {
+            'intermediate':False,
+            'data-link':False,
+            'basic':False,
+            'max payload':0
+        }
+        self.timeout = timeout
+        return
+    def __update_log(self,msg):
+        self.log.put(msg)
+        return
+    def get_log(self):
+        ts = []
+        msgs = []
+        while not self.log.empty():
+            t,msg = self.log.get().split(':')
+            ts.append(t)
+            msgs.append(msg)
+        df = pd.DataFrame({'time':ts, 'event':msgs})
+        df.set_index('time',inplace=True)
+        return df
+    def __write(self,msg,log=False,end='\n'):
+        if log:
+            self.__update_log(msg)
+        print(msg,end=end)
+        return
+    def __recv(self,verbose=True):
+        res = None
+        try:
+            res = self.com.get_next_msg()
+            if res != None:
+                t_e = time.time()
+                msg,t = res # check against timeout
+                t = int(t)
+                if time.time() - t >= self.timeout:
+                    self.__write(f"{t}: waiting for response timeout!",log=True)
+                    raise TimeoutException('Timeout!')
+                res = self.cta_mod.from_cta(msg)
+                if type(res) == dict:
+                    self.__write(f"{t}: received {res['command']}",log=True)
+                    if verbose:
+                        for k,v in res['args'].items():
+                            self.__write(f"\t{k} = {v}")
+                if 'op1' in res:
+                    self.last_command = res['op1']
+        except UnsupportedCommandException as e:
+            self.__write(f"{t}: received Unsupported Command -- {e.message}",log=True)
+            raise UnsupportedCommandException(msg) # propagate exception
+        except UnknownCommandException as e:
+            self.__write(f"{t}: received Unknwon command -- {msg}",log=True)
+            raise UnknownCommandException(msg) # propagate exception
+        return res
+    def __send(self,cmd,args={},verbose=False):
+        ret = False
+        c = self.cta_mod.to_cta(cmd,args=args)
+        self.com.send(c)
+        self.__write(f'{int(time.time())}: sent {cmd}',log=True)
+        if verbose:
+            if len(args) > 0:
+                self.__write('\twith args:')
+                for k,v in args.items():
+                    if not v[0].isalpha():
+                        v = v.replace(' 0x','')
+                        v = int(v,16)
+                    self.__write(f'\t{k}: {v}')
+        ret = True # else an exception would be raised and this statement would be skipped when unwinding the stack
+        return ret
+    def __setup(self):
+        res = None
+        success=False
+        cmds = [('intermediate mtsq','intermediate'),
+                ('data-link mtsq','data-link'),
+                ('max payload request','max payload'),
+                ('basic mtsq','basic')
+                ]
+        for cmd,flag in cmds:
+            try:
+                #cmd = 'intermediate mtsq'
+                if self.__send(cmd):
+                    # wait for response
+                    res = self.__recv()
+                if res != None and 'command' in res and res['command'] == 'ack':
+                    self.support_flags[flag] = True
+                if 'max' in cmd:
+                    res = self.__recv() # capture max payload
+                    if res != None and 'args' in res and 'max' in res['command']:
+                        self.__send('ack')
+                        length = res['args']['max_payload_length']
+                        self.support_flags[flag] = int(length)
+            except TimeoutException:
+                flag = False
+            except UnsupportedCommandException  as e:
+                pass
+        self.__write(str(self.support_flags))
+        success = self.support_flags['intermediate'] and self.support_flags['data-link'] and self.support_flags['max payload'] > 0
+        return success
+    def __prompt(self,valid=False):
+        '''
+            outputs a prompt message to the screen
+
+        '''
+        if not valid:
+            self.__write(f"{'-'*5}> INVALID. Try again!\n")
+            self.prompted = False
+        msg = "select a command:\n"
+        for k,v in choices.items():
+            msg += f"{k}: {v}\n"
+        self.__write(msg)
+        self.__write("enter a choice: ")
+        return
+    # ------------------------- DCM Loop ----------------------------------
+    # -----------------------------------------------------------------------------
+    def __run_dcm(self):
+        valid = True
+        
+        # sent unsupported commands -- doesn't make sense for DCM to ack any of them
+        self.cta_mod.set_supported('shed',False)
+        self.cta_mod.set_supported('endshed',False)
+        self.cta_mod.set_supported('loadup',False)
+        self.cta_mod.set_supported('grid emergency',False)
+        self.cta_mod.set_supported('critical peak event',False)
+        # run daemon
+        proc = multiprocessing.Process(target=self.__run_daemon)
+        proc.daemon = True
+        proc.start()
+        choice = ''
+        # prompt_time = 30 # every 30 secs will prompt
+        # next_prompt = time.time() + prompt_time
+        last_sz = self.log.qsize()
+        self.__prompt(valid)
+        while 1:
+            if last_sz+2 <= self.log.qsize(): # if log has changed by 2 msgs
+                time.sleep(.5*self.timeout) # sleep to allow log msgs to be printed
+                self.__prompt(valid)
+                last_sz = self.log.qsize() # update last_log size
+            i = select.select([sys.stdin],[],[],0) # 0=> disables blocking mode
+            if i[0]:
+                try:
+                    choice = sys.stdin.read(1) # read 1 char
+                    sys.stdin.flush()
+                    choice = int(choice) # try to cast it
+                    valid = choice in choices
+                    choice = '' if not valid else choices[choice]
+                except (KeyError,ValueError) as e:
+                    valid = False
+                    self.__prompt(valid)
+                    choice = ''
+                    continue
+            if choice == 'quit':
+                proc.terminate()
+                return
+            elif choice != '':
+                self.__send(choice) # sent command, daemon receives and responds to the command
+                choice = ''
+            valid = True
+        return
+    # ------------------------- Daemon Loop ----------------------------------
+    # -----------------------------------------------------------------------------
+    def __run_daemon(self):
+        while 1:
+            args = {}
+            try:
+                res = self.__recv() # always waiting for commands
+                if res != None:
+                    cmd = res['command']
+                    # invoke model with request command -- if supported
+                    if cmd in self.FDT:
+                        args = res['args']
+                        args = self.FDT[cmd](payload=args)
+                    complement = self.cta_mod.complement(cmd)
+                    for cmd in complement:
+                        if cmd == 'app ack':
+                            self.__send(cmd, {'last_opcode':self.last_command})
+                        elif cmd == 'nak':
+                            self.__send(cmd,{'nak_reason':'unsupported'})
+                        else:
+                            self.__send(cmd,args)
+            except TimeoutException as e:
+                # nothing was received from UCM
+                pass
+            except (UnsupportedCommandException,UnknownCommandException) as e:
+                self.__send('nak',{'nak_reason':'unsupported'})
+            except KeyboardInterrupt as e:
+                break # exit loop & return
+        return
+    def run(self):
+        self.com.start()
+        self.__setup()
+        if self.mode == 'DER':
+            # validate model in DER mode
+            assert(isinstance(self.model,CTA2045Model))
+            self.FDT = {
+                'shed':self.model.shed,
+                'endshed':self.model.endshed,
+                'loadup':self.model.loadup,
+                'operating status request':self.model.operating_status,
+                'commodity read request':self.model.commodity_read,
+                'grid emergency':self.model.grid_emergency,
+                'critical peak event':self.model.critical_peak_event
+            }
+            self.__run_daemon()
+        elif self.mode=='DCM':
+            self.FDT = {} # empty it
+            self.__run_dcm()
+        else:
+            raise UnknownModeException(f'Unknown Mode: {self.mode}')
+        return self.get_log()
