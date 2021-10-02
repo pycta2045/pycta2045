2209208069bfe94f4e96516553f0d8050826c0bc
mohamm-alsaid <mohamm.alsaid@gmail.com>
2021-10-01 19:23:41 -0700

    Remove unnecessary dependancies in dcm.py

--- examples/dcm.py
+++ examples/dcm.py
@@ -6,7 +6,6 @@ This demonstrates how use the simple UCM provided by pycta2045 library.
 import pandas as pd, argparse as ap, select, sys,os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from pycta2045 import cta2045device as device
-from rich import pretty,print
 
 
 def main():
@@ -14,7 +13,6 @@ def main():
     parser.add_argument('-p',required=False,type=str,help="com port to use for connection. e.g: -p /dev/ttyS2", default='/dev/ttyS2')
     args = parser.parse_args()
     port = args.p
-    pretty.install()
     # default mode of operation is DCM
     dev = device.CTA2045Device(comport=port)
     dev.run(block=True)
